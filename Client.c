83d945beca3da66c949d2152883ff6420f6de99c
Morrie0125 <134068323+Morrie0125@users.noreply.github.com>
2025-01-16 18:36:26 -0800

    UDP Client

--- Client.c
+++ Client.c
@@ -1,56 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
-#include <stdlib.h>     // exit, atoi
-#include <string.h>     // bzero, strlen
-#include <unistd.h>     // read, write, close
-void error(char *msg)
+#include <netdb.h>
+
+void error(const char *msg)
 {
     perror(msg);
-    exit(0);
+    exit(1);
 }
 
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
-
+    socklen_t serverlen;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    char buffer[1024];
 
-    char buffer[256];
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+       exit(1);
     }
+
+    // 1) 建立 Datagram socket (UDP)
     portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
         error("ERROR opening socket");
+
+    // 2) 解析主機名 -> IP
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+        exit(1);
     }
+
+    // 3) 設定要發送的目標位址 (server IP + port)
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+
+    serverlen = sizeof(serv_addr);
+
+    // 4) 輸入要傳給伺服器的資料
     printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
+    bzero(buffer,1024);
+    fgets(buffer,1023,stdin);
+
+    // 5) 用 sendto() 發送資料到 server
+    n = sendto(sockfd, buffer, strlen(buffer), 0,
+               (struct sockaddr *)&serv_addr, serverlen);
+    if (n < 0)
+      error("ERROR in sendto");
+
+    // 6) 用 recvfrom() 接收伺服器回覆
+    bzero(buffer, 1024);
+    n = recvfrom(sockfd, buffer, 1024, 0,
+                 (struct sockaddr *)&serv_addr, &serverlen);
+    if (n < 0)
+      error("ERROR in recvfrom");
+
+    printf("Server reply: %s\n", buffer);
+
+    close(sockfd);
     return 0;
 }
